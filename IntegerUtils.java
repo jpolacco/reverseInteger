c7a24442fc194a71a879a346f64c1728229302c9
jpolacco <jpolacco@gmail.com>
2026-07-10 14:37:37 -0700

    Update IntegerUtils.java

--- IntegerUtils.java
+++ IntegerUtils.java
@@ -16,37 +16,11 @@ public class IntegerUtils {
      *
      * @param n the integer to reverse
      * @return the reversed integer
-     * @throws ArithmeticException if the reversed integer overflows
      */
-    public static int reverse(int n) {
-
-        int reversed = 0;
-
-        while (n != 0) {  // n = -54 
-
-            int digit = n % 10; // 4, -5
-            n /= 10; // n = -5 , 0 
-
-            // Check for positive overflow
-            if (reversed > Integer.MAX_VALUE / 10 ||
-                (reversed == Integer.MAX_VALUE / 10 &&
-                 digit > Integer.MAX_VALUE % 10)) {
-
-                throw new ArithmeticException("Integer overflow");
-            }
-
-            // Check for negative overflow
-            if (reversed < Integer.MIN_VALUE / 10 ||
-                (reversed == Integer.MIN_VALUE / 10 &&
-                 digit < Integer.MIN_VALUE % 10)) {
-
-                throw new ArithmeticException("Integer overflow");
-            }
-
-            reversed = reversed * 10 + digit;  // reversed = 4
-            // 4*10 - 5
-        }
-
-        return reversed;
+    public int reverse(int n) {
+        // TO_DO: Your code here.
+        // Complete the method using arithmetic operators and a loop
+        // You may NOT simply convert the int to a String or StringBuilder and reverse it
+        
     }
 }
