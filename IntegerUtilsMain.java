d0d7137c38e78f0245c47c7a0085afb2648b4bc4
jpolacco <jpolacco@gmail.com>
2026-08-24 15:08:24 -0700

    Update IntegerUtilsMain.java

--- IntegerUtilsMain.java
+++ IntegerUtilsMain.java
@@ -10,8 +10,6 @@ public class IntegerUtilsMain
        IntegerUtils utils = new IntegerUtils(); // instantiate an IntegerUtils object
        System.out.println("The reverse of 123 is " + utils.reverse(123));
        // TO-DO: Write at least two more test cases here.
-       System.out.println("The reverse of 30100 is " + utils.reverse(30100));
-      System.out.println("The reverse of -2005 is " + utils.reverse(-2005));
    }
 }
 
